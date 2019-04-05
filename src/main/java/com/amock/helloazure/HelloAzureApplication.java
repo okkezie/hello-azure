45be9c0d7698550edcaf65cd74d48cf61abf2bb9
Okezie Arukwe <okezie.arukwe@etranzact.com>
2019-04-05 12:05:35 +0100

    initial commit

---
+++ src/main/java/com/amock/helloazure/HelloAzureApplication.java
@@ -0,0 +1,13 @@
+package com.amock.helloazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAzureApplication.class, args);
+	}
+
+}
