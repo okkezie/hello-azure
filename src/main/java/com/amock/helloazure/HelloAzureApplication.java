d8aef044c1ee98de5e64c5737264844027fed6a0
Okezie Arukwe <okezie.arukwe@etranzact.com>
2019-04-05 13:05:07 +0100

    added homepage view and controller

--- src/main/java/com/amock/helloazure/HelloAzureApplication.java
+++ src/main/java/com/amock/helloazure/HelloAzureApplication.java
@@ -2,6 +2,8 @@ package com.amock.helloazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class HelloAzureApplication {
@@ -10,4 +12,12 @@ public class HelloAzureApplication {
 		SpringApplication.run(HelloAzureApplication.class, args);
 	}
 
+	@Controller
+	class WebController {
+		
+		@GetMapping
+		public String home(){
+			return "index";
+		}
+	}
 }
